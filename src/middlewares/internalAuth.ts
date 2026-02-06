27929cdfa9581fe0c2566f7ab5b7cc70b8150c00
davihenriquedev1 <davihenrique.ads@gmail.com>
2026-02-05 22:49:01 -0300

    fix(api): route protection adjustment and authentication validation
    on branch main
    
    changes to be submitted:
    
    modified:   src/middlewares/internalAuth.ts

--- src/middlewares/internalAuth.ts
+++ src/middlewares/internalAuth.ts
@@ -3,7 +3,9 @@ import { NextFunction, Request, Response } from "express";
 export function internalAuth(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['x-api-key'];
 
-    if(!apiKey || apiKey !== process.env.INTERNAL_API_KEY) {
+    const key = Array.isArray(apiKey) ? apiKey[0] : apiKey;
+
+    if(!key || key !== process.env.INTERNAL_API_KEY) {
         return res.status(401).json({error: 'unauthorized'});
     }
     next();
